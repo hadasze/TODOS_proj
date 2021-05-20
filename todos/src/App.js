66fa1784941787ce1b985d6fdf4052245a1f93ed
hadasze <82802761+hadasze@users.noreply.github.com>
2021-05-20 12:30:36 +0300

    Revert "Todo component"

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,45 +1,14 @@
-import { useEffect, useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Todo from './Components/Todo'
 
 function App() {
 
-  const [enteredTodoLabel, setEnteredTodoLabel] = useState('stam');
-  const [idCounter, setIdCounter] = useState(0);
-//  {id: number, label: label, active: true}
-  const [allTodos, setAllTodos] = useState([]);
-
-
-  const submitCompleteTodo = (currID) => {
-    allTodos.forEach(todo => { todo.id === currID ? todo.active = false : todo.active = todo.active;
-    });
-  }
-
-  const submitInProgressTodo = (currID) => {
-    allTodos.forEach(todo => { todo.id === currID ? todo.active = true : todo.active = todo.active;
-    });
-  }
-
-  const addTodo = () => {
-    console.log("dd");
-    setIdCounter(idCounter+1);
-    setAllTodos ( [...allTodos, {id: idCounter, label:enteredTodoLabel, active: true}])  
-  }
-
-  useEffect(() => {
-
-  }, [allTodos]);
 
   return (
     <div className="App">
-      <input type="text" name="todoLabel" id="label" onChange={(e) => setEnteredTodoLabel(e.target.value)} value={enteredTodoLabel}/> 
-      <button onClick={addTodo}> ADD TODO</button>
-      
-      <h2>all todos</h2>
-      {allTodos ? allTodos.map((currTodo) => 
-              <Todo id={currTodo.id} label={currTodo.label} submitCompleteTodo={submitCompleteTodo} submitInProgressTodo={submitInProgressTodo}></Todo>
-      ) : ""}
-      
+      <input></input>
+      <Todo></Todo>
     </div>
   );
 }
--- todos/src/Components/Todo.js
+++ todos/src/Components/Todo.js
@@ -1,27 +1,14 @@
-import React, {useState} from 'react'
 
 
 function Todo(props) {
 
-    const [complete, setComplete] = useState(false);
-
-    const onCompleteHandler = () => {
-        // setComplete(true);
-        props.submitCompleteTodo()
-    }
-
-    const onActiveHandler = () => {
-        // setCheck(!check);
-        props.submitInProgressTodo();
-    }
-
 
     return (
       <form>
-          {/* <input type="checkbox"> {complete ? "inProgress" : "complete"}</input> */}
-          <input type="checkbox" name="news" id="news" checked={props.active} onChange={props.active ? () => onActiveHandler(props.id) : 
-                                                                                                        () => onCompleteHandler(props.id)}/> 
-          {props.label}
+          <href>
+          <button>button</button>
+          todo
+          </href>
       </form>
     );
   }
